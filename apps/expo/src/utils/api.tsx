0c798d7cb183af115789f23f3ff70ba3c2d4b43e
LaunchThatBrand <info@launchthatbrand.com>
2023-04-18 17:29:21 -0400

    moved some clerk files over, builds locally

--- apps/expo/src/utils/api.tsx
+++ apps/expo/src/utils/api.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Constants from "expo-constants";
+import { useAuth } from "@clerk/clerk-expo";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import superjson from "superjson";
 
 import { type AppRouter } from "@acme/api";
+import { transformer } from "@acme/api/transformer";
 
 /**
  * A set of typesafe hooks for consuming your API.
@@ -43,12 +45,19 @@ const getBaseUrl = () => {
 export const TRPCProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const { getToken } = useAuth();
   const [queryClient] = React.useState(() => new QueryClient());
   const [trpcClient] = React.useState(() =>
     api.createClient({
       transformer: superjson,
       links: [
         httpBatchLink({
+          async headers() {
+            const authToken = await getToken();
+            return {
+              Authorization: authToken ?? undefined,
+            };
+          },
           url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+import { UserButton, useAuth } from "@clerk/nextjs";
 
 import { api, type RouterOutputs } from "~/utils/api";
 
@@ -135,27 +138,44 @@ const Home: NextPage = () => {
 export default Home;
 
 const AuthShowcase: React.FC = () => {
-  const { data: session } = api.auth.getSession.useQuery();
-
+  const { isSignedIn } = useAuth();
   const { data: secretMessage } = api.auth.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: !!session?.user },
+    undefined,
+    { enabled: !!isSignedIn },
   );
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      {session?.user && (
+      {isSignedIn && (
+        <>
+          <p className="text-center text-2xl text-white">
+            {secretMessage && (
+              <span>
+                {" "}
+                {secretMessage} click the user button!
+                <br />
+              </span>
+            )}
+          </p>
+          <div className="flex items-center justify-center">
+            <UserButton
+              appearance={{
+                elements: {
+                  userButtonAvatarBox: {
+                    width: "3rem",
+                    height: "3rem",
+                  },
+                },
+              }}
+            />
+          </div>
+        </>
+      )}
+      {!isSignedIn && (
         <p className="text-center text-2xl text-white">
-          {session && <span>Logged in as {session?.user?.name}</span>}
-          {secretMessage && <span> - {secretMessage}</span>}
+          <Link href="/sign-in">Sign In</Link>
         </p>
       )}
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={session ? () => void signOut() : () => void signIn()}
-      >
-        {session ? "Sign out" : "Sign in"}
-      </button>
     </div>
   );
 };

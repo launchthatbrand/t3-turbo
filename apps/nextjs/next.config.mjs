5a70503d720c06c07ce9a120a69582e729fd5cad
Desmond <desmond@Desmonds-MacBook-Pro.local>
2023-04-20 17:21:21 -0400

    added @acme/app and moved nextjs homescreen

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,17 +1,30 @@
+import { withExpo } from "@expo/next-adapter";
+
+// @ts-check
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
- * This is especially useful for Docker builds and Linting.
+ * This is especially useful for Docker builds.
  */
-// !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+// !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
-/** @type {import("next").NextConfig} */
-const config = {
-  reactStrictMode: true,
-  /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@acme/api", "@acme/auth", "@acme/db"],
-  /** We already do linting and typechecking as separate tasks in CI */
-  eslint: { ignoreDuringBuilds: !!process.env.CI },
-  typescript: { ignoreBuildErrors: !!process.env.CI },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // reanimated (and thus, Moti) doesn't work with strict mode currently...
+  // https://github.com/nandorojo/moti/issues/224
+  // https://github.com/necolas/react-native-web/pull/2330
+  // https://github.com/nandorojo/moti/issues/224
+  // once that gets fixed, set this back to true
+  reactStrictMode: false,
+  transpilePackages: [
+    "react-native",
+    "react-native-web",
+    "solito",
+    "moti",
+    "app",
+    "react-native-reanimated",
+    "nativewind",
+    "react-native-gesture-handler",
+  ],
 };
 
-export default config;
+export default withExpo(nextConfig);
---
+++ packages/app/features/home/screen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { TextLink } from "solito/link";
+
+export function HomeScreen() {
+  return (
+    <View>
+      <TextLink href="/">THIS IS HOMESCREEN</TextLink>
+    </View>
+  );
+}
